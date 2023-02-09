cdbebe96c80f3df92e614531625101fb851db1da
zhanshijun <2644278773@qq.com>
2023-02-09 11:17:42 +0800

    Initial commit

---
+++ springwork/src/main/java/com/example/springwork/SpringworkApplication.java
@@ -0,0 +1,13 @@
+package com.example.springwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringworkApplication.class, args);
+    }
+
+}
